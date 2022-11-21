fa0228ab9d666b9d92c12f1345e2309b08d5c932
AlvaroAGDuoc <a.aguerog@duocuc.cl>
2022-11-20 23:35:01 -0300

    terminada

--- home/static/js/dispositivo.js
+++ home/static/js/dispositivo.js
@@ -1,20 +1,16 @@
-
-  
 const $boton = document.getElementById('botonDescarga');
 const $descarga = document.getElementById('descarga');
+const $qr = document.getElementById('qr');
 
 
 (function () {
     $boton.addEventListener('click', function(e) {
         let navegador = navigator.userAgent;
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-            $descarga.innerHTML = "<h1>MOVIL</h1> <p>aaaaaaaaaaaaaa</p>"
-            e.preventDefault()
-        } else {
-            $descarga.innerHTML = "<h1>PC</h1> <p>aaaaaaaaaaaaaa</p>"
-            e.preventDefault()
+            $descarga.innerHTML = "<a class='link' href='https://drive.google.com/uc?export=download&id=1ckEFY8vcs6NzVj7dH9A41fVi1SGbvCti' target='_blank'>Link de descarga</a>"      
+        } else {      
+            $qr.removeAttribute("hidden")            
         }
     })
-
 })();
 
